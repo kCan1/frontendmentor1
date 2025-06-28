3e1a2131bf039d01b7e8f9e6226f2d3b63ddfda5
KpG782 <kenpatrickgarcia123@gmail.com>
2025-06-28 22:33:09 +0800

    sample tailwind try

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}", // adjust depending on your folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
